85014303efea5b791c6379a84c8279b3ccbab734
Lastramus <bananowiec2222@gmail.com>
2016-03-11 11:35:37 +0100

    stworzone modele

---
+++ UWB/application/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    login = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    index_number = models.CharField(max_length=45)
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    login = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+
+class Classes(models.Model):
+    name = models.CharField(max_length=45)
+    lecturer = models.ForeignKey('Lecturer')
+
+class Attendance(models.Model):
+    date = models.DateTimeField()
+    attend = models.BooleanField(default=False)
+    student = models.ForeignKey('Student')
+    lecturer = models.ForeignKey('Lecturer')
+    classes = models.ForeignKey('Classes')
+
+class Mark(models.Model):
+    value = models.FloatField()
+    student = models.ForeignKey('Student')
+    lecturer = models.ForeignKey('Lecturer')
+    classes = models.ForeignKey('Classes')
+
