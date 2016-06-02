fa7cc1884852515018b62a221bb3c6f891adb96d
Lastramus <bananowiec2222@gmail.com>
2016-06-02 23:06:30 +0200

    tabela dla wyswietlania danych

--- UWB/application/static/js/profile.js
+++ UWB/application/static/js/profile.js
@@ -4,22 +4,19 @@ function executeQuery() {
     success: function(data) {
         collected_data = jQuery.parseJSON(data)['collected_data'];
 
-        $('#stage').html('<p> Dane dla użytkownika: ' + collected_data.student_name  + ' </p>' + '<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%"><thead><tr><th>Kurs</th><th>Prowadzący</th><th>Data</th><th>Obecność</th></tr></thead><tbody>');
+        $('#stage').html('<p> Dane dla użytkownika: ' + collected_data.student_name  + ' </p>')
+        $('#stage').append('<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%"><thead><tr><th>Kurs</th><th>Prowadzący</th><th>Data</th><th>Obecność</th></tr></thead><tbody></tbody></table>');
         for (i = 0; i < collected_data.student_data.length; i++) {
 
             if(collected_data.student_data[i].attendance) {
+                console
 
-                $('#stage').append(
-		'<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>')
-
+                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>')
             }
             else {
-                $('#stage').append(
-		'<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>')
+                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>')
             }
         }
-	$('#stage').append('</tbody></table>');
-
     }
   });
   setTimeout(executeQuery, 5000); 
