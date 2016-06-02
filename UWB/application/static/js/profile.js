c3199e24361d7c6d1416f264a77d2101f253b802
jszynalski <200906@student.pwr.edu.pl>
2016-06-03 01:09:07 +0200

    modyfikacje do layoutu v2

--- UWB/application/static/js/profile.js
+++ UWB/application/static/js/profile.js
@@ -4,17 +4,18 @@ function executeQuery() {
     success: function(data) {
         collected_data = jQuery.parseJSON(data)['collected_data'];
 
-        $('#stage').html('<p> Dane dla użytkownika: ' + collected_data.student_name  + ' </p>')
-        $('#stage').append('<table id="table" class="table table-striped table-bordered" cellspacing="0" width="100%"><thead><tr><th>Kurs</th><th>Prowadzący</th><th>Data</th><th>Obecność</th></tr></thead><tbody></tbody></table>');
+        $('#stage').html('<h4> Dane dla użytkownika: <b>' + collected_data.student_name  + '</b><h4>')
+        $('#stage').append('<table id="table" class="table table-condensed table-bordered" width="100%"><thead><tr><th>Kurs</th><th>Prowadzący</th><th>Data</th><th>Obecność</th></tr></thead><tbody></tbody></table>');
+
         for (i = 0; i < collected_data.student_data.length; i++) {
 
             if(collected_data.student_data[i].attendance) {
                 console
-
-                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>')
+                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>');
             }
+
             else {
-                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>')
+                $('#table').append('<tr><td>' + collected_data.student_data[i].classes_name + '</td><td>' + collected_data.student_data[i].lecturer_name + '</td><td>' + collected_data.student_data[i].attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>');
             }
         }
     }
--- UWB/application/static/js/staff_profile.js
+++ UWB/application/static/js/staff_profile.js
@@ -3,23 +3,24 @@ function executeQueryForAll() {
     url: '/uwb/all_json/',
     success: function(data) {
         collected_data = jQuery.parseJSON(data)['collected_data'];
-        $('#stage').html('<p> Dane dla użytkownika: ' + collected_data.lecturer_name  + ' </p>');
+
+        $('#stage').html('<h4> Dane dla użytkownika: <b>' + collected_data.lecturer_name  + '</b></h4>');
+
         for (i = 0; i < collected_data.lecturer_data.length; i++) {
-            $('#stage').append('<p> Dane dla zajęć: '
-             + collected_data.lecturer_data[i].classes_name + ' </p>');
+
+            $('#stage').append('<h4> Dane dla zajęć: <b>' + collected_data.lecturer_data[i].classes_name + '</b></h4>')
+	    $('#stage').append('<table id="table" class="table table-condensed table-bordered" width="100%"><thead><tr><th>#</th><th>Imię</th><th>Nazwisko</th><th>Nr Indeksu</th><th>Data</th><th>Obecność</th></tr></thead><tbody></tbody></table>');
+
             for (j = 0; j < collected_data.lecturer_data[i].classes_data.length; j++) {
                 student_data = collected_data.lecturer_data[i].classes_data[j]
+
                 if(student_data.attendance) {
-                    $('#stage').append('<p> ' + (j + 1) + '. Student: ' +
-                student_data.first_name + ' ' + student_data.last_name + ' ' + 
-                student_data.index_number + ' ' + student_data.attendance_date + 
-                '<font color="green"> Obecny </font>' +' </p>');
+		console
+                    $('#table').append('<tr><td>' + (j + 1) + '</td><td>' + student_data.first_name + '</td><td>' + student_data.last_name + '</td><td>' + student_data.index_number + '</td><td>' + student_data.attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>');
                 }
+
                 else {
-                    $('#stage').append('<p> ' + (j + 1) + '. Student: ' +
-                student_data.first_name + ' ' + student_data.last_name + ' ' + 
-                student_data.index_number + ' ' + student_data.attendance_date + 
-                '<font color="red"> Nieobecny </font>' +' </p>');
+                    $('#table').append('<tr><td>' + (j + 1) + '</td><td>' + student_data.first_name + '</td><td>' + student_data.last_name + '</td><td>' + student_data.index_number + '</td><td>' + student_data.attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>');
                 }
             }
         }
@@ -33,23 +34,24 @@ function executeQueryForProfile() {
     url: '/uwb/profile_json/',
     success: function(data) {
         collected_data = jQuery.parseJSON(data)['collected_data'];
-        $('#stage').html('<p> Dane dla użytkownika: ' + collected_data.lecturer_name  + ' </p>');
+
+        $('#stage').html('<h4> Dane dla użytkownika: <b>' + collected_data.lecturer_name  + '</b></h4>');
+
         for (i = 0; i < collected_data.lecturer_data.length; i++) {
-            $('#stage').append('<p> Dane dla zajęć: '
-             + collected_data.lecturer_data[i].classes_name + ' </p>');
+
+            $('#stage').append('<h4> Dane dla zajęć: <b>' + collected_data.lecturer_data[i].classes_name + '</b></h4>')
+	    $('#stage').append('<table id="table" class="table table-condensed table-bordered" width="100%"><thead><tr><th>#</th><th>Imię</th><th>Nazwisko</th><th>Nr Indeksu</th><th>Data</th><th>Obecność</th></tr></thead><tbody></tbody></table>');
+
             for (j = 0; j < collected_data.lecturer_data[i].classes_data.length; j++) {
                 student_data = collected_data.lecturer_data[i].classes_data[j]
+
                 if(student_data.attendance) {
-                    $('#stage').append('<p> ' + (j + 1) + '. Student: ' +
-                student_data.first_name + ' ' + student_data.last_name + ' ' + 
-                student_data.index_number + ' ' + student_data.attendance_date + 
-                '<font color="green"> Obecny </font>' +' </p>');
+		console
+                    $('#table').append('<tr><td>' + (j + 1) + '</td><td>' + student_data.first_name + '</td><td>' + student_data.last_name + '</td><td>' + student_data.index_number + '</td><td>' + student_data.attendance_date + '</td><td>' + '<font color="green"> Obecny </font>' + '</td></tr>');
                 }
+
                 else {
-                    $('#stage').append('<p> ' + (j + 1) + '. Student: ' +
-                student_data.first_name + ' ' + student_data.last_name + ' ' + 
-                student_data.index_number + ' ' + student_data.attendance_date + 
-                '<font color="red"> Nieobecny </font>' +' </p>');
+                    $('#table').append('<tr><td>' + (j + 1) + '</td><td>' + student_data.first_name + '</td><td>' + student_data.last_name + '</td><td>' + student_data.index_number + '</td><td>' + student_data.attendance_date + '</td><td>' + '<font color="red"> Nieobecny </font>' + '</td></tr>');
                 }
             }
         }
